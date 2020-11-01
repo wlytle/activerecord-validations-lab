9431c328a90c7e78fcb7b734be85dded3caebd0b
wlytle <wlytle89@gmail.com>
2020-11-01 10:49:09 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 205 }
+  validates :category, inclusion: { in: %w(Fiction Nonfiction), message: "%{value} is not a valid category " }
+  validate :clickbaity
+
+  def clickbaity
+    if title.present? && !title.match?("Won't Believe" || "Secret" || "Top /\d/")
+      errors.add(:not_clickbaity_enough, "Yo Dawg, jazz it up. I don't even want to click this!")
+    end
+  end
 end
